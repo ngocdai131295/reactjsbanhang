bdecdfc2016683851eaf78d5506eb3ece2454b94
ngocdai131295 <72639681+ngocdai131295@users.noreply.github.com>
2022-03-25 17:40:45 +0700

    a

--- src/Routers.js
+++ src/Routers.js
@@ -5,8 +5,12 @@ import Signin from './user/Signin';
 import Home from './core/Home';
 // import Menu from './core/Menu';
 
+// user
 import PrivateRoute from './auth/PrivateRoute';   //trang bảo mật route nếu ko đăng nhập  thì sẽ chuyển hướng
 import Dashboard from './user/UserDashboard'
+//admin dashboard
+import AdminDashboard from './user/AdminDashboard ';
+import AdminRoute from './auth/AdminRoute';
 
 const Routes = () => {
     return (
@@ -19,7 +23,7 @@ const Routes = () => {
 
                 {/* click vào dashboard chưa đăng nhập thì đc chuyển hướng đến đăng nhập */}
                 <PrivateRoute path="/user/dashboard" exact component={Dashboard}/>
-                {/* <AdminRoute path="/admin/dashboard" exact component={AdminDashboard} /> */}
+                <AdminRoute path="/admin/dashboard" exact component={AdminDashboard} />
             </Switch>
         </BrowserRouter>
     );
